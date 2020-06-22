d00f99c242f9613bf56dfc60365c1460b795f8db
vashu <vashu.yadav65@gmail.com>
2020-06-22 18:43:48 +0530

    blog post layout added

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Auth::routes();
+
+Route::get('/', 'HomeController@index')->name('home');
